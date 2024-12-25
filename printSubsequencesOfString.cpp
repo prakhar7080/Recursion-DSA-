f03128977156cc3268055d964418d5c38852fc3f
prakhar7080 <prakhargupta3510@gmail.com>
2024-12-25 18:36:14 +0530

    Create printSubsequencesOfString.cpp

---
+++ printSubsequencesOfString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printSub(string str, string substr, int index){
+    if(index == str.length()){
+        cout<<substr<<endl;
+        return;
+    }
+    char ch = str[index];
+    printSub(str,substr + ch, index+1);
+    printSub(str,substr,index+1);
+}
+int main(){
+    string str = "abc";
+    string substr = "";
+    int index = 0;
+    printSub(str,substr,index);
+}
